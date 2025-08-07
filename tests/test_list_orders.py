4f625604e83c3c5d8b871f38f089aae4421bd2b5
Stas Puzachev <stanislavpuzachev@yandex.ru>
2025-08-07 11:53:50 +0300

    Добавил with allure.step

--- tests/test_list_orders.py
+++ tests/test_list_orders.py
@@ -7,5 +7,11 @@ from urls import Urls
 class TestReturnOrderList:
     @allure.title('В тело ответа возвращается список заказов')
     def test_list_order(self):
-        response = requests.get(f'{Urls.URL}{Handle.CREATE_ORDER}')
-        assert response.status_code == 200 and "orders" in response.json()
+
+        with allure.step("Отправка GET-запроса для получения списка заказов"):
+            response = requests.get(f'{Urls.URL}{Handle.GET_ORDERS_LIST}')  # Важно убедиться, что endpoint верный!
+
+
+        with allure.step("Проверка статуса ответа и содержания поля orders"):
+            assert response.status_code == 200
+            assert "orders" in response.json()
