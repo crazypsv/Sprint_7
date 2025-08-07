5d2dd6ca64f061c2026db41fe9229ccdd6dae0dd
Stas Puzachev <stanislavpuzachev@yandex.ru>
2025-08-07 11:48:27 +0300

    Добавил with allure.step

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -9,11 +9,22 @@ from urls import Urls
 
 class TestCreateOrder:
 
-    @pytest.mark.parametrize('order_data', [{"color": ["BLACK"]}, {"color": ["GREY"]}, {"color": [""]}, {"color": ["BLACK", "GREY"]}])
+    @pytest.mark.parametrize('order_data',
+                             [{"color": ["BLACK"]}, {"color": ["GREY"]}, {"color": [""]}, {"color": ["BLACK", "GREY"]}])
     @allure.title('Создание заказа')
     def test_create_order(self, order_data):
+
         Orders.data_order.update(order_data)
-        order_data = json.dumps(Orders.data_order)
+
+        order_json = json.dumps(Orders.data_order)
+
         headers = {'Content-Type': 'application/json'}
-        response = requests.post(f'{Urls.URL}{Handle.CREATE_ORDER}', data=order_data, headers=headers)
-        assert response.status_code == 201 and 'track' in response.text
+
+
+        with allure.step("Отправка POST-запроса на создание заказа"):
+            response = requests.post(f'{Urls.URL}{Handle.CREATE_ORDER}', data=order_json, headers=headers)
+
+
+        with allure.step("Анализируем статус-код и содержание ответа"):
+            assert response.status_code == 201
+            assert 'track' in response.text
